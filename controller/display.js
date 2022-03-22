e109e42d66b45332405e4fcaf5e5a230336bbb44
Ramiro González Maciel <ramirogm@gmail.com>
2022-03-22 20:55:10 -0300

    FIX display init when no i2cbus device present

--- controller/display.js
+++ controller/display.js
@@ -7,47 +7,33 @@
 //
 
 
-var i2c = require('i2c-bus'),
-  i2cBus = i2c.openSync(1),
-  oled = require('oled-i2c-bus');
+const i2c = require('i2c-bus');
+const oled = require('oled-i2c-bus');
 
-var font = require('oled-font-5x7');
+const font = require('oled-font-5x7');
 
-var opts = {
+const opts = {
   width: 128,
   height: 64,
   address: 0x3C
 };
 
-var oled = new oled(i2cBus, opts);
-
-
+var oledDisplay;
 
 function startDisplay() {
-  oled.turnOnDisplay();
-  oled.clearDisplay();    
-  oled.setCursor(1, 1);
-  oled.writeString(font, 2, 'FinRadio', 1, false);
+  const i2cBus = i2c.openSync(1);
+  oledDisplay = new oled(i2cBus, opts);
+  oledDisplay.turnOnDisplay();
+  oledDisplay.clearDisplay();    
+  oledDisplay.setCursor(1, 1);
+  oledDisplay.writeString(font, 2, 'FinRadio', 1, false);
 }
 
 function displayFrequency(freqInMhz) {
-  oled.setCursor(1, 30);
+  oledDisplay.setCursor(1, 30);
   const msg = ( freqInMhz < 100 ? " " : "" ) + freqInMhz.toFixed(2) + " Mhz";
-  oled.writeString(font, 2, msg, 1, false);
+  oledDisplay.writeString(font, 2, msg, 1, false);
 }
-
-// draws 4 white pixels total
-// format: [x, y, color]
-// oled.drawPixel([
-// 	[128, 1, 1],
-// 	[128, 32, 1],
-// 	[128, 16, 1],
-// 	[64, 16, 1]
-// ]);
-
-// oled.fillRect(1, 1, 128, 64, 1);
-
-// oled.drawLine(1, 1, 128, 32, 1);
 
 module.exports = {
   startDisplay,
