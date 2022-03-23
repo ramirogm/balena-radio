0eafd2a1499a699cb3f60e840d921228d9bf1765
Ramiro González Maciel <ramirogm@gmail.com>
2022-03-22 23:24:20 -0300

    keyreader

--- keyreader/index.js
+++ keyreader/index.js
@@ -1,45 +1,77 @@
 const dgram = require('dgram');
-const Buffer = require('buffer').Buffer;
-
-
-const RADIO_HOSTNAME = process.env["RADIO_HOSTNAME"] || "192.168.0.97";
-const RADIO_PORT = process.env["RADIO_PORT"] || "6020";
-
-
-
+const { Buffer } = require('buffer');
+const { hrtime } = require('process');
 const readline = require('readline');
+
+
+const listenerPort = process.env.LISTENER_PORT || '8001';
+const listenerAddress = process.env.LISTENER_ADDRESS || 'localhost';
+const logEnabled = "1" === process.env.LOG_ENABLED || true;
+const delta = 1;
+
+const clientSocket = dgram.createSocket('udp4');
+
+let clicked = false;
+let lastClickTime = hrtime.bigint();
+
+let counter = 0;
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
 
 process.stdin.on('keypress', (str, key) => {
-
+  logEnabled && console.log(`keypress key: ${key}`);
   if (key.ctrl && key.name === 'c') {
     process.kill(process.pid, 'SIGINT')
-  }  
-  console.log(str);
-  console.log(key);
-  if ( key.name == 'right' || key.name == 'left' ) {
-    sendCommand(key.name);
   }
+  let direction;
+  let value;
+  if (key.name === process.env.RIGHT_KEY) {
+    direction = 'R';
+    value = delta
+  } else if (key.name === process.env.LEFT_KEY) {
+    direction = 'L';
+    value = -delta;
+  } else if (key.name === process.env.PUSH_KEY) {
+    direction = 'C';
+    value = 0;
+  } else {
+    return;
+  }
+  sendClickInfo(direction, value);
 })
 
 
 // Mouse events you said? Check https://github.com/TooTallNate/keypress
 
-
-// 
-function sendCommand(name) {
-  const message = Buffer(1);
-  message[0] = name == "left" ? 4 : name == "right" ? 5 : null;
-  const client = dgram.createSocket('udp4');
-  console.log(`Sending message ${JSON.stringify(message)} to ${RADIO_HOSTNAME}:${RADIO_PORT}`)
-  client.send(message, RADIO_PORT, RADIO_HOSTNAME, (err) => {
-    client.close();
+function sendClickInfo(event, value) {
+  counter = value;
+  const lapseInMs = lapseFromLastClick();
+  const message = Buffer.alloc(9);
+  message.writeUint8(event.charCodeAt(0), 0);
+  message.writeInt32BE(value, 1);
+  message.writeInt32BE(lapseInMs, 5);
+  clientSocket.send(message, listenerPort, listenerAddress, (err) => {
+    if ( err ) {
+      console.error("Error when sending packet", err)
+    }
   });
-
 }
 
+function lapseFromLastClick() {
+  if ( !clicked ) {
+    clicked = true;
+    lastClickTime = hrtime.bigint();
+    return 0;
+  } else {
+    const now = hrtime.bigint();
+    const dif = now - lastClickTime;
+    const difInt = Number(dif);
 
-
+    lastClickTime = now;
+    const lapseInMs = difInt / 1000000;
+    logEnabled && console.log(`Time between clicks: ${lapseInMs} milliseconds`);
+    return lapseInMs;
+  }
+}
